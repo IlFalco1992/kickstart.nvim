5bc504a09721c6b9a79446b47658474c4dc0ee3c
Francesco Scotti <francesco.scotti92@gmail.com>
2024-04-08 17:21:56 +0200

    awk lsp

--- init.lua
+++ init.lua
@@ -542,6 +542,7 @@ require('lazy').setup({
         gopls = {},
         phpactor = {},
         pyright = {},
+        awk_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
