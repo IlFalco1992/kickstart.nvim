00d9248ed9a87691b84b617a39e869a0d060d73a
Francesco Scotti <francesco.scotti92@gmail.com>
2024-04-11 09:18:31 +0200

    aerial

--- init.lua
+++ init.lua
@@ -872,10 +872,20 @@ require('lazy').setup({
         pre_save_cmds = {
           function()
             require('dapui').close()
+            require('aerial').close()
           end,
         },
       }
     end,
+  },
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -1015,3 +1025,18 @@ require('dap.ext.vscode').load_launchjs 'launch.json'
 -- SPELL CHECK
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
+
+-- AERIAL
+require('aerial').setup {
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+  end,
+}
+-- You probably also want to set a keymap to toggle aerial
+-- vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
+vim.keymap.set('n', '<leader>a', function()
+  require('aerial').toggle { direction = 'left', focus = false }
+end, { desc = 'Aerial [a] toggle' })
