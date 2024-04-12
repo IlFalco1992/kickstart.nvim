6676915d245ddb2016c2448d30699100c24aa897
Francesco Scotti <francesco.scotti92@gmail.com>
2024-04-12 10:50:18 +0200

    sqlls

--- init.lua
+++ init.lua
@@ -548,6 +548,7 @@ require('lazy').setup({
         awk_ls = {},
         jsonls = {},
         html = {},
+        sqlls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
