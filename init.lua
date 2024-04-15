f4ae368b9dd8d48ce769fd9275450f835398a05d
Francesco Scotti <francesco.scotti92@gmail.com>
2024-04-15 19:31:08 +0200

    telescope jumplist

--- init.lua
+++ init.lua
@@ -379,6 +379,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>j', builtin.jumplist, { desc = '[J] Jumplist' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
