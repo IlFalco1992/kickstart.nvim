f2d456fbab0f6ef95cd0f894566c162ed083f9a5
Francesco Scotti <francesco.scotti92@gmail.com>
2024-04-18 17:12:01 +0200

    avoid closing the dap ui on terminaion

--- init.lua
+++ init.lua
@@ -980,12 +980,12 @@ end
 dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited.dapui_config = function()
+--   dapui.close()
+-- end
 
 -- DAP Golang
 require('dap-go').setup {
