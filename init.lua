0e52c6e6a1cfcf0956bbd706c6c43bdb82709f20
Francesco Scotti <francesco.scotti92@gmail.com>
2024-04-21 20:26:11 +0200

    <leader>h for signature help

--- init.lua
+++ init.lua
@@ -505,6 +505,9 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Show the signature of the function call you're currently on.
+          map('<leader>h', vim.lsp.buf.signature_help, 'Signature [H]elp')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
