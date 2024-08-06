4390e3f19a4b03ef7075e6763ba0fa1da2562aee
Francesco Scotti <francesco.scotti92@gmail.com>
2024-08-06 17:52:07 +0100

    new auto-session

--- init.lua
+++ init.lua
@@ -896,6 +896,16 @@ require('lazy').setup({
   { 'Weissle/persistent-breakpoints.nvim' },
   {
     'rmagatti/auto-session',
+    lazy = false,
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    keys = {
+      -- Will use Telescope if installed or a vim.ui.select picker otherwise
+      { '<leader>wr', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+      { '<leader>ws', '<cmd>SessionSave<CR>', desc = 'Save session' },
+      { '<leader>wa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
+    },
     config = function()
       require('auto-session').setup {
         log_level = 'error',
@@ -905,6 +915,12 @@ require('lazy').setup({
             require('dapui').close()
             require('aerial').close()
           end,
+        },
+        session_lens = {
+          -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+          load_on_setup = true,
+          theme_conf = { border = true },
+          previewer = false,
         },
       }
     end,
