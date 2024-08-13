f682928b1c19ba6bb28bbd703d7d0cfe2429041e
Francesco Scotti <francesco.scotti92@gmail.com>
2024-08-13 17:48:59 +0200

    js DAP

--- init.lua
+++ init.lua
@@ -944,6 +944,7 @@ require('lazy').setup({
     lazy = false, -- This plugin is already lazy
   },
   { 'kmonad/kmonad-vim' },
+  { 'mxsdev/nvim-dap-vscode-js' },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -1075,6 +1076,36 @@ dap.configurations.php = {
     },
   },
 }
+
+-- DAP VSCODE JS
+require('dap-vscode-js').setup {
+  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+  debugger_path = os.getenv 'HOME' .. '/vscode-js-debug/', -- Path to vscode-js-debug installation.
+  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+  -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+  -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+  -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+}
+
+for _, language in ipairs { 'typescript', 'javascript' } do
+  require('dap').configurations[language] = {
+    {
+      type = 'pwa-node',
+      request = 'launch',
+      name = 'Launch file',
+      program = '${file}',
+      cwd = '${workspaceFolder}',
+    },
+    {
+      type = 'pwa-node',
+      request = 'attach',
+      name = 'Attach',
+      processId = require('dap.utils').pick_process,
+      cwd = '${workspaceFolder}',
+    },
+  }
+end
 
 -- DAP launch.json
 require('dap.ext.vscode').load_launchjs 'launch.json'
