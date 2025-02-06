1fd20e59413aec1ec2ee20ed9fd6fee6652acc1f
Francesco Scotti <francesco.scotti92@gmail.com>
2025-02-06 14:42:24 +0000

    Revert "gitsigns new options"
    This reverts commit d493767fe10913751b589ecbbbd590d6229a467a.

--- init.lua
+++ init.lua
@@ -250,15 +250,15 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = function()
+    opts = {
       signs = {
         add = { text = '+' },
         change = { text = '~' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
-      }
-    end,
+      },
+    },
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
