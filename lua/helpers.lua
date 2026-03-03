890d58039d2b4f9832f551c0acac4ee61627f7a0
Francesco Scotti <francesco.scotti92@gmail.com>
2026-03-03 16:07:56 +0000

    EscapeRegex refactored

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -1,18 +1,5 @@
-EscapeRegex = function(text)
-  text = string.gsub(text, '\\', '\\\\')
-  text = string.gsub(text, '%.', '\\.')
-  text = string.gsub(text, '%*', '\\*')
-  text = string.gsub(text, '%+', '\\+')
-  text = string.gsub(text, '%?', '\\?')
-  text = string.gsub(text, '%^', '\\^')
-  text = string.gsub(text, '%$', '\\$')
-  text = string.gsub(text, '%(', '\\(')
-  text = string.gsub(text, '%)', '\\)')
-  text = string.gsub(text, '%[', '\\[')
-  text = string.gsub(text, '%]', '\\]')
-  text = string.gsub(text, '%{', '\\{')
-  text = string.gsub(text, '%}', '\\}')
-  return text
+function EscapeRegex(text)
+  return (text:gsub('([\\%(%)%.%%%+%-%*%?%[%]%^%$])', '\\%1'))
 end
 
 -- TELESCOPE delete buffers from list (https://medium.com/@jogarcia/delete-buffers-on-telescope-21cc4cf61b63)
